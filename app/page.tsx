f9faa510013e84b1b699524880944bcd2e21901d
King Jian Rong <jkin0029@student.monash.edu>
2025-01-03 15:00:42 +0800

    Added stack

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,14 @@
+import { Hero } from "@/components/Hero";
 import { Navbar } from "@/components/Navbar";
+import { Stack } from "@/components/Stack";
 
 
 export default function Home() {
   return (
     <>
       <Navbar />
+      <Hero />
+      <Stack />
     </>
   );
 }
